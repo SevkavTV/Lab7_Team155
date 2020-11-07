694cd4adcbfc3d024fcda8f02709f0fccef70f67
Pr0digg <impr0digg@gmail.com>
2020-11-07 13:13:26 +0200

    Added lives, number type choosing, window is ready to being imported

--- ui/start_window.py
+++ ui/start_window.py
@@ -1,10 +1,24 @@
-from tkinter import *
-window = Tk()
-TK_SILENCE_DEPRECATION = 1
-window.title('A POOO')
-window.geometry('500x500+500+200')
-window.resizable(False, False)
-start_game_label = Label(window, text="Hello Tkinter!")
-start_game_label.pack()
+import tkinter
 
-window.mainloop()
+window2 = tkinter.Tk()
+window2.geometry('500x500')
+# window1 = game_window.get_window()
+# window2.withdraw()
+frame = tkinter.Frame(window2)
+frame.grid(row=1, column=1)
+frame.pack(expand=True)
+start_btn = tkinter.Button(frame, text="123", fg='black')
+start_btn.grid(column=1, row=1)
+
+
+def click():
+    window2.destroy()  # Close current window
+    import game_window  # Import game window
+    game_window.lives = 10  # Setting all game vars
+    game_window.start_window()  # Launchung game window
+
+
+start_btn['command'] = click
+
+
+tkinter.mainloop()
